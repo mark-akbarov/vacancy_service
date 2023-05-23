7911a1c3a95b83a14d0bd19f9cd9f718552dbc99
mark-akbarov <markakbarov@gmail.com>
2023-05-23 22:43:22 +0500

    reinitiate project

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for config project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -20,4 +20,3 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
